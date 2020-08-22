c7657e2f691051aebf81e7d551e2ee359f04572a
GustavoBabarone <gugabruder@gmail.com>
2020-08-22 15:27:10 -0300

    Testando git config

--- D-memoria-arrays-listas/Program.cs
+++ D-memoria-arrays-listas/Program.cs
@@ -4,6 +4,7 @@ namespace D_memoria_arrays_listas
 {
     class Program
     {
+        // testando git config
         static void Main(string[] args)
         {
             Console.WriteLine("Quarta Parte do Curso de C#!");
