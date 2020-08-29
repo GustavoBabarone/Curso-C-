7630ca1c8ed63579731e33528abae49c826d6ecd
GustavoBabarone <gugabruder@gmail.com>
2020-08-29 15:18:06 -0300

    Adicionando aulas sobre Herança e Polimorfismo

---
+++ G-heranca-polimorfismo/Aula01/Entities/ContaSalario.cs
@@ -0,0 +1,24 @@
+﻿namespace G_heranca_polimorfismo.Aula01.Entities
+{
+    public class ContaSalario : Conta
+    {
+        public double LimiteEmprestimo { get; set; }
+
+        public ContaSalario()
+        {
+        }
+
+        public ContaSalario(int numero, string titular, double saldo, double limiteEmprestimo) : base(numero, titular, saldo)
+        {
+            LimiteEmprestimo = limiteEmprestimo;
+        }
+
+        public void RealizarEmprestimo(double quantia)
+        {
+            if (quantia >= LimiteEmprestimo)
+            {
+                Saldo += quantia;
+            }
+        }
+    }
+}
---
+++ G-heranca-polimorfismo/Aula02/Entities/ContaCorrente.cs
@@ -0,0 +1,21 @@
+﻿namespace G_heranca_polimorfismo.Aula02.Entities
+{
+    public class ContaCorrente : Conta
+    {
+        public double TaxaJuros { get; set; }
+
+        public ContaCorrente()
+        {
+        }
+
+        public ContaCorrente(int numero, string titular, double saldo, double taxaJuros) : base(numero, titular, saldo)
+        {
+            TaxaJuros = taxaJuros;
+        }
+
+        public void AtualizarSaldo()
+        {
+            Saldo += Saldo * TaxaJuros;
+        }
+    }
+}
